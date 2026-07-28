d4910abc444625cf5631ef72bef5d1931eefc023
Sagar Bhattarai <bhattaraisagar2001@gmail.com>
2026-07-28 16:30:17 +0545

    Update API base URL to Render backend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://pdf-converter-backend-r81c.onrender.com',
 })
 
 export const convertFile = (file, type) => {
